8752d4d601d6904092ccfcc2b8d622b620c1aed9
Tarasov Maxim <tarasovm2013@gmail.com>
2021-04-18 20:53:31 +0300

    add android + keyboard service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {KeyboardService} from './@core/services/keyboard.service';
+import {Platform} from '@ionic/angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+    @ViewChild('appWindow', {static: true}) private appWindow: ElementRef;
+
+    constructor(
+        private keyboardService: KeyboardService,
+        private platform: Platform,
+    ) {}
+
+    public ngOnInit(): void {
+        this.initializeApp();
+    }
+
+    private initializeApp(): void {
+        this.platform.ready().then(() => {
+            this.keyboardService.setInitSettings(this.platform, this.appWindow).then();
+        });
+    }
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {NavController, PopoverController} from "@ionic/angular";
-import {CarPopowerComponent} from "./components/car-popower/car-popower.component";
+import {NavController, PopoverController} from '@ionic/angular';
+import {KeyboardService} from '../../@core/services/keyboard.service';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 
 @Component({
@@ -9,22 +11,40 @@ import {CarPopowerComponent} from "./components/car-popower/car-popower.componen
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit, OnDestroy {
+    @ViewChild('content') private content: ElementRef;
     public loginForm: FormGroup = new FormGroup({
         login: new FormControl('', Validators.required),
         pass: new FormControl('', Validators.required),
     });
-
+    private subscriber$: Subject<null> = new Subject<null>();
     private readonly nextUrl: string = 'tabs';
 
     constructor(
         public popoverController: PopoverController,
         private navCtrl: NavController,
+        private keyboardService: KeyboardService,
     ) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.keyboardService.keyboardHeight$
+            .pipe(takeUntil(this.subscriber$))
+            .subscribe(this.scrollToBottom.bind(this));
+    }
+
+    ngOnDestroy(): void {
+        this.subscriber$.next(null);
+        this.subscriber$.complete();
+    }
 
     public submit(e: Event): void {
         this.navCtrl.navigateRoot(this.nextUrl).then();
+    }
+
+    private scrollToBottom(): void {
+        if (!this.content?.nativeElement) {
+            return;
+        }
+        this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
     }
 }
