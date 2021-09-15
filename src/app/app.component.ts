8fd11d33ba20e469aa9b5b1dc6724286b6114cba
KMatevosyan <71639310+KMatevosyan@users.noreply.github.com>
2021-09-15 21:14:52 +0700

    16.09.2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import {Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {KeyboardService} from './@core/services/platform/keyboard.service';
-import {Platform} from '@ionic/angular';
-import {ThemeService} from './services/theme.service';
-import {DOCUMENT} from '@angular/common';
-import {UserInfoService} from './services/user-info.service';
-import {Subscription} from 'rxjs';
-import {NfcService} from "./@core/services/platform/nfc.service";
-import {TabsInfoService} from "./services/tabs/tabs-info.service";
-
-import * as GK from 'gauss-krueger';
-import {GeoProjection, toFlat, toWGS} from "as-geo-projection";
+import {
+    Component,
+    ElementRef,
+    Inject,
+    OnDestroy,
+    OnInit,
+    ViewChild,
+} from '@angular/core';
+import { KeyboardService } from './@core/services/platform/keyboard.service';
+import { Platform } from '@ionic/angular';
+import { ThemeService } from './services/theme.service';
+import { DOCUMENT } from '@angular/common';
+import { UserInfoService } from './services/user-info.service';
+import { Subscription } from 'rxjs';
+import { NfcService } from './@core/services/platform/nfc.service';
+import { TabsInfoService } from './services/tabs/tabs-info.service';
+import { GeoProjection, toFlat, toWGS } from 'as-geo-projection';
 
 @Component({
     selector: 'app-root',
@@ -17,7 +22,7 @@ import {GeoProjection, toFlat, toWGS} from "as-geo-projection";
     styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-    @ViewChild('appWindow', {static: true}) private appWindow: ElementRef;
+    @ViewChild('appWindow', { static: true }) private appWindow: ElementRef;
     private subscription: Subscription;
     private timeOut: any;
     constructor(
@@ -34,13 +39,12 @@ export class AppComponent implements OnInit, OnDestroy {
         this.initializeApp();
         this.themeService.setThemeConfiguratorRoot(this.document).then();
 
-        this.subscription = this.userInfo.statusId$.subscribe(value => {
+        this.subscription = this.userInfo.statusId$.subscribe((value) => {
             const time = 25 * 60000;
             if (value !== 2) {
                 clearTimeout(this.timeOut);
                 this.timeOut = null;
-            }
-            else {
+            } else {
                 if (!this.timeOut) {
                     this.timeOut = setTimeout(async () => {
                         this.userInfo.openActivityModal().then();
@@ -49,20 +53,29 @@ export class AppComponent implements OnInit, OnDestroy {
             }
         });
 
-        const lt = toFlat({latitude: 55.0982678796, longitude: 73.1202739448});
-        const rb = toFlat({latitude: 55.0643063117, longitude: 73.2950520486});
+        const lt = toFlat({
+            latitude: 55.0982678796,
+            longitude: 73.1202739448,
+        });
+        const rb = toFlat({
+            latitude: 55.0643063117,
+            longitude: 73.2950520486,
+        });
         const geo = new GeoProjection();
         geo.setBorders({
             wgs: {
-                lt: {latitude: 55.0982678796, longitude: 73.1202739448},
-                rb: {latitude: 55.0643063117, longitude: 73.2950520486},
+                lt: { latitude: 55.0982678796, longitude: 73.1202739448 },
+                rb: { latitude: 55.0643063117, longitude: 73.2950520486 },
             },
             flat: {
-                lt: {x: 8139711.6621998055, y: 7380961.309697426,},
-                rb: {x: 8159167.871716635, y: 7374356.672722412,},
+                lt: { x: 8139711.6621998055, y: 7380961.309697426 },
+                rb: { x: 8159167.871716635, y: 7374356.672722412 },
             },
         });
-        const res = geo.getRelativeByWgs({latitude: 55.0982678796, longitude: 73.2202739448});
+        const res = geo.getRelativeByWgs({
+            latitude: 55.0982678796,
+            longitude: 73.2202739448,
+        });
     }
 
     public ngOnDestroy(): void {
@@ -71,7 +84,11 @@ export class AppComponent implements OnInit, OnDestroy {
 
     private initializeApp(): void {
         this.platform.ready().then(() => {
-            setTimeout(() => this.keyboardService.setInitSettings(this.platform, this.appWindow).then());
+            setTimeout(() =>
+                this.keyboardService
+                    .setInitSettings(this.platform, this.appWindow)
+                    .then()
+            );
             setTimeout(() => this.tasksService.cancelData(), 1000);
         });
         this.nfcService.initNfc();
