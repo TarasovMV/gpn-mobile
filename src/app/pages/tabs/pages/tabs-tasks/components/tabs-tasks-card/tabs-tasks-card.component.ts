8f3e5224fe5edb26049d9c6ef3407429a02778b8
Tarasov Maxim <tarasovm2013@gmail.com>
2021-09-27 09:24:52 +0300

    fix: counter

--- src/app/pages/tabs/pages/tabs-tasks/components/tabs-tasks-card/tabs-tasks-card.component.ts
+++ src/app/pages/tabs/pages/tabs-tasks/components/tabs-tasks-card/tabs-tasks-card.component.ts
@@ -16,10 +16,18 @@ import { ITask } from '../../../../../../@core/model/task.model';
     styleUrls: ['./tabs-tasks-card.component.scss'],
 })
 export class TabsTasksCardComponent implements OnInit, AfterViewInit {
-    @Input() data: ITask;
-    @Input() isActive: boolean;
-
+    public taresCount: number = 0;
+    public probesCount: number = 0;
     public percent$: BehaviorSubject<number> = new BehaviorSubject<number>(20);
+    @Input() isActive: boolean;
+    @Input() public set data(value: ITask) {
+        this._data = value;
+        this.taresCount = value.tares.map(x => x.count).reduce((acc, next) => acc + next, 0);
+        this.probesCount = value.probes.map(x => x.count).reduce((acc, next) => acc + next, 0);
+    }
+    public get data(): ITask {return this._data;}
+    private _data: ITask;
+
     constructor() {}
 
     ngOnInit() {}
